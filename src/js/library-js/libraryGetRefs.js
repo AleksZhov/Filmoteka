3e363730e10c65995c137e095cf7d0a4e1e6861d
AleksZhov <alekszhov@gmail.com>
2022-07-24 21:52:19 +0300

    Added modal to library

--- src/js/library-js/libraryGetRefs.js
+++ src/js/library-js/libraryGetRefs.js
@@ -1,8 +1,12 @@
 const libraryGetRefs = () => {
   return {
+    modal: document.querySelector('.modal'),
     containerListRef: document.querySelector('.container__list'),
-    watchedBthRef: document.querySelector('.js-btn-watched'),
-    queueBtnRef: document.querySelector('.js-btn-queue'),
+    modalCloseBtnRef: document.querySelector('.modal__close'),
+    modalFilm: document.querySelector('.modal__film'),
+    modalContainer: document.querySelector('.modal__conteiner'),
+    watchedBthRef: document.querySelector('.js-btn-watched-render'),
+    queueBtnRef: document.querySelector('.js-btn-queue-render'),
   };
 };
 export default libraryGetRefs;
