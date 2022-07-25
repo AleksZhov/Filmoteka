e0b6547e2c69cd5613f44817fc76df339e241063
Oleg Gnatyk <103590840+yavenir@users.noreply.github.com>
2022-07-25 16:04:56 +0300

    Remove preloader

--- src/js/preloader.js
+++
@@ -1,8 +0,0 @@
-import { renderModalMarkup } from './renderModalMarkup';
-import getRefs from './getRefs.js';
-const preloader = document.getElementById('preloader');
-
-window.addEventListener('load', () => {
-  preloader.style.display = 'none';
-  getRefs().body.classList.remove('overflow');
-});
--- src/library-index.js
+++ src/library-index.js
@@ -1,5 +1,5 @@
 import './js/team-modal.js';
-import './js/preloader.js';
 import './js/library-js/library.js';
 import './js/library-js/library-queue.js';
 import './js/library-js/libraryOpenModal.js';
+import './js/scroll-up.js';
